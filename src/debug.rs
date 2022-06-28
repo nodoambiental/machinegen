91ad00e31bad6c698fa8a0dc38afd0ca472b397e
Ágata Ordano <ordanoagata@gmail.com>
2022-06-28 18:47:10 -0300

    feat(debug): add table debug statement

--- src/debug.rs
+++ src/debug.rs
@@ -1,1 +1,9 @@
-pub fn run(_sub_match: &clap::ArgMatches) {}
+use super::types::TableTypes;
+use super::util;
+
+pub fn run(_sub_match: &clap::ArgMatches) {
+    // Test table parsing
+    // println!("{:?}", util::load_table(TableTypes::Replace));
+    // println!("{:?}", util::load_table(TableTypes::Files));
+    // println!("{:?}", util::load_table(TableTypes::Template));
+}
